40f37345713d801f091251ff5f26241ab1a2e547
YsmaelSG <ysmaelsandoval096@gmail.com>
2025-11-09 03:04:01 -0500

    i finsihed the backend help me lord

--- ASMRAI/backend/app/main.py
+++ ASMRAI/backend/app/main.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
 from fastapi import HTTPException
@@ -7,13 +8,23 @@ from pydantic import BaseModel
 import asyncio
 from dotenv import load_dotenv
 from google import genai
+from my_agent.agent import root_agent
+
+
 # loads env variables into os enviroment to be SNATCHED
 load_dotenv() 
 api_curr_key = os.getenv("GEMINI_API_KEY")
 
 class Video(BaseModel):
     response: str
-    orderNumber: int
+
+def add_asmr_to_text(txt: str) -> str:
+    txt = txt.strip()
+    if "asmr" not in txt.lower():
+        return f"{txt} asmr"
+    else:
+        return txt
+    
 
 
 client = genai.Client(api_key=api_curr_key)
@@ -22,9 +33,15 @@ app = FastAPI()
 
 @app.post("/")
 async def getVideo(videoBody: Video):
+
+    textInModels = add_asmr_to_text(videoBody.response)
+    modelObject = root_agent.run(textInModels)
+    finalText = f"make the video {modelObject.duration_sec} long and {modelObject.aspect_ratio} long . Prompt: {modelObject.prompt}"
+    
+
     operation = client.models.generate_videos(
         model ="veo-3.1-generate-preview",
-        prompt= str(videoBody.response),)
+        prompt= str(finalText))
     
     while not operation.done:
         await asyncio.sleep(2)
